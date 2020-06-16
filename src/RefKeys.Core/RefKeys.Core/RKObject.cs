ce9229c79470ce5ff91cee9c84e5be85a745deb3
Sam Parkaleh <MentalCodeMaster@hotmail.com>
2020-06-16 14:53:45 +0430

    Create RKObject.cs

---
+++ src/RefKeys.Core/RefKeys.Core/RKObject.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace RefKeys.Core
+{
+    public class RKObject : INotifyPropertyChanged
+    {
+
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
